6ea6eb16277c464c8ad20ef6d77f8602f6c0a734
denis9diaz <150247992+denis9diaz@users.noreply.github.com>
2024-02-08 20:44:24 +0000

    cambios styles

--- src/js/component/Buttons.js
+++ src/js/component/Buttons.js
@@ -3,9 +3,9 @@ import React from "react";
 const Buttons = (props) => {
     return (
         <div className="buttons">
-            <button type="button" className="buttonStop btn btn-outline-light me-2" onClick={props.stopCounter}>Stop</button>
-            <button type="button" className="buttonRestart btn btn-outline-light me-2" onClick={props.restartCounter}>Restart</button>
-            <button type="button" className="buttonReset btn btn-outline-light" onClick={props.resetCounter}>Reset</button>
+            <button type="button" className="buttonStop btn btn-outline-light me-2" onClick={props.stopCounter}>STOP</button>
+            <button type="button" className="buttonRestart btn btn-outline-light me-2" onClick={props.restartCounter}>RESTART</button>
+            <button type="button" className="buttonReset btn btn-outline-light" onClick={props.resetCounter}>RESET</button>
         </div>
     )
 }
